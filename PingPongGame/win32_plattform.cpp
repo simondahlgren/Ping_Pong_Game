c084b493e856e89aaca5be270f65b164853b41ba
Simon Dahlgren <sida2000@student.miun.se>
2022-07-28 23:41:39 +0200

    added game window

--- PingPongGame/win32_plattform.cpp
+++ PingPongGame/win32_plattform.cpp
@@ -1,8 +1,24 @@
 #include <windows.h>
+
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+
 	WNDCLASS window_class = {};
-	window_class.style = CS_HREDRAW
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	//window_class.lpszClassName = "test";
+	window_class.lpfnWndProc = window_callback;
+	RegisterClass(&window_class);
+	CreateWindow("hej","Mitt första spel", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+
 }
+
+
+
 
 
