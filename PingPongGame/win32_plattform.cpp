ec4e063180196da5159787f40af395d8f70e6d16
Simon Dahlgren <sida2000@student.miun.se>
2022-07-28 23:52:19 +0200

    .

--- PingPongGame/win32_plattform.cpp
+++ PingPongGame/win32_plattform.cpp
@@ -1,8 +1,10 @@
 #include <windows.h>
 
+bool running = true;
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
@@ -11,11 +13,14 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 	WNDCLASS window_class = {};
 	window_class.style = CS_HREDRAW | CS_VREDRAW;
-	//window_class.lpszClassName = "test";
+	window_class.lpszClassName = L"Game Window Class";
 	window_class.lpfnWndProc = window_callback;
 	RegisterClass(&window_class);
-	CreateWindow("hej","Mitt första spel", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	CreateWindow(window_class.lpszClassName,L"Mitt första spel", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	while (running)
+	{
 
+	}
 }
 
 
