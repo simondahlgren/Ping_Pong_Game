cf5b5fcd9040984abe78f52de01891753e66ee69
Simon Dahlgren <sida2000@student.miun.se>
2022-07-28 22:52:12 +0200

    Add project files.

---
+++ PingPongGame/win32_plattform.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW
+}
+
+
