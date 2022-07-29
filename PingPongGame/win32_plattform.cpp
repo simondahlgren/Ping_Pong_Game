2d7600053b9eebf24e85d3166a60eed7454a379b
Simon Dahlgren <sida2000@student.miun.se>
2022-07-29 19:50:43 +0200

    added window for application

--- PingPongGame/win32_plattform.cpp
+++ PingPongGame/win32_plattform.cpp
@@ -4,8 +4,21 @@ bool running = true;
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	LRESULT result = 0;
 
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	switch (uMsg)
+	{
+	case WM_CLOSE: 
+	case WM_DESTROY: {
+		running = false;
+	} break;
+
+	default: {
+		return result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
+	}
+	return result;
 }
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
@@ -16,9 +29,15 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	window_class.lpszClassName = L"Game Window Class";
 	window_class.lpfnWndProc = window_callback;
 	RegisterClass(&window_class);
-	CreateWindow(window_class.lpszClassName,L"Mitt första spel", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HWND window = CreateWindow(window_class.lpszClassName,L"Mitt första spel", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
 	while (running)
 	{
+		MSG message; 
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
 
 	}
 }
